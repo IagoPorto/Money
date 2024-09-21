7697ca4bfc57ef7ea68a000920071a4a8c06b1af
IagoPorto <iagopm8@gmail.com>
2024-09-21 09:36:46 +0200

    starting side project

---
+++ money/src/test/java/com/sideproject/money/money/MoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.money.money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
